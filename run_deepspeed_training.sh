9c752fc44db70240d9e784db5f527f891e09185d
wuminye <huanlekf@163.com>
2025-08-27 21:35:18 +0200

    Update run_deepspeed_training.sh

--- run_deepspeed_training.sh
+++ run_deepspeed_training.sh
@@ -27,8 +27,8 @@ echo "Additional arguments: $@"
 # batch_size will be automatically calculated based on micro_batch_size and world_size
 deepspeed --num_gpus=$NUM_GPUS train_deepspeed.py \
     --deepspeed_config ../deepspeed_config.json \
-    --batch_size 32 \
-    --micro_batch_size 8 \
+    --batch_size 24 \
+    --micro_batch_size 6 \
     --learning_rate 2e-4 \
     --max_iters 200000 \
     --wandb_project "graspgpt-deepspeed" \
