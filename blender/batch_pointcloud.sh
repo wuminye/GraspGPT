2ba4732350a7a038e1754c96dba81ee90994cc89
wuminye <huanlekf@163.com>
2025-09-16 13:03:33 +0200

    Update batch_pointcloud.sh

--- blender/batch_pointcloud.sh
+++ blender/batch_pointcloud.sh
@@ -1,5 +1,5 @@
 python obj_to_pointcloud.py --voxel_size 0.0075 --input_dir ../output/synthetic_meshes
 python obj_to_pointcloud.py --voxel_size 0.0075 --input_dir ../output/synthetic_meshes_part2
-python obj_to_pointcloud.py --voxel_size 0.0075 --input_dir ../output/synthetic_meshes_part3 --skips 3000
+python obj_to_pointcloud.py --voxel_size 0.0075 --input_dir ../output/synthetic_meshes_part3 
 python obj_to_pointcloud.py --voxel_size 0.0075 --input_dir ../output/synthetic_meshes_part4
 python obj_to_pointcloud.py --voxel_size 0.0075 --input_dir ../output/synthetic_meshes_part5
